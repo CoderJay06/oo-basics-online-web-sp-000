c5c004ed77df891d27742a19b0691d2eeacbd530
Jason J Cruz <jaycru06@yahoo.com>
2019-11-29 21:40:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -30,6 +30,26 @@ class Shoe
   
   def material 
     @material 
+  end
+  
+  def condition=(condition)
+    @condition
+  end 
+  
+  def condition
+    @condition 
   end 
   
 end 
+
+
+
+
+
+
+
+
+
+
+
+
